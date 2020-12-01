3c090ca778a75de86956daafe998e5255b920e60
Manisha Choudhary <manisha_choudhary@hotmail.com>
2020-12-01 12:37:59 -0500

    Navbar component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import  MyNavbar  from "./components/mynavbar.components";
+
+const App=() => {
+  return (
+    <div>
+      <MyNavbar/>
+      My Portfolio
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/mynavbar.components.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from "../assets/icons/logo.jpg";
+import "./mynavbar.styles.css";
+
+
+const MyNavbar = () => {
+  return(
+  <div>
+    <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between" >
+      <div>
+        <Navbar.Brand href="#home">
+          <img className="Logo" src={Logo} alt="My Portfolio Logo" />
+        </Navbar.Brand>
+      </div>
+      <div>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </div>
+    </Navbar>
+  </div>
+    )  
+}
+
+export default MyNavbar;
